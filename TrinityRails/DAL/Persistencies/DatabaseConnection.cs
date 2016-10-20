bb83f4af84976ca5d3e923bb4adacf358f5255ee
Stijn <stijn.vanderlinden@student.fontys.nl>
2016-10-20 11:54:43 +0200

    V0.1.3.1
    Features:
    - DatabaseConnection ipv namespace RepositoryPattern, nu DAL

--- TrinityRails/DAL/Persistencies/DatabaseConnection.cs
+++ TrinityRails/DAL/Persistencies/DatabaseConnection.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
 
-namespace RepositoryPattern.Persistencies
+namespace DAL.Persistencies
 {
     public class DatabaseConnection
     {
---
+++ TrinityRails/DAL/Persistencies/TramSQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DAL.Interfaces;
+using DAL.Types;
+
+namespace DAL.Persistencies
+{
+    public class TramSQL : ITram
+    {
+        private DatabaseConnection databaseConnection;
+        public TramSQL()
+        {
+            databaseConnection = new DatabaseConnection();
+        }
+        public List<Tram> getTrams()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setStatus(Tram tram, Status status)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLocation(Tram tram, int location)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
