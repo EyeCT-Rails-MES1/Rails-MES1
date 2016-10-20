17799c6129921f84fea47f23ab4ce28a28a2d85e
Stijn <stijn.vanderlinden@student.fontys.nl>
2016-10-20 15:15:11 +0200

    V0.2.7
    Feature:
    - EngineerSQL created

---
+++ TrinityRails/DAL/Persistencies/EngineerSQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using DAL.Types;
+using Classes.Enumerations;
+
+namespace DAL.Persistencies
+{
+    class EngineerSQL : IEngineer
+    {
+        private DatabaseConnection databaseConnection;
+
+        public EngineerSQL()
+        {
+            databaseConnection = new DatabaseConnection();
+        }
+
+        public void setDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setStatus()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
