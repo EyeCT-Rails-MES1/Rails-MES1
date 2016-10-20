2a74a717a3a5fa03cd6a6cf7ee8b8ded0b2d1266
Stijn <stijn.vanderlinden@student.fontys.nl>
2016-10-20 14:22:09 +0200

    V0.2.3
    Feature:
    - AdminSQL created

---
+++ TrinityRails/DAL/Persistencies/AdminSQL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using DAL.Types;
+using Classes.Enumerations;
+
+namespace DAL.Persistencies
+{
+    class AdminSQL : IAdmin
+    {
+        private DatabaseConnection databaseConnection;
+        public AdminSQL()
+        {
+            databaseConnection = new DatabaseConnection();
+        }
+
+        public void changePassword()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void changeUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void create()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
