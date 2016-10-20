18583b685fb166631d6b94810b9a60759607cb06
Luuk van der Kallen <luuk-vdk@live.nl>
2016-10-20 15:20:22 +0200

    V0.2.8
    Features:
    CleanerRepository created

---
+++ TrinityRails/DAL/Repositories/CleanerRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using DAL.Types;
+using Classes.Enumerations;
+using Classes;
+
+namespace DAL.Repositories
+{
+    public class CleanerRepository
+    {
+        ICleaner context;
+
+        public CleanerRepository(ICleaner context)
+        {
+            this.context = context;
+        }
+        public List<string> getCleaningList()
+        {
+            throw new NotImplementedException();
+        }
+        public void finishCleaning()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
