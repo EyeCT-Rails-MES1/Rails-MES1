f3cfc0e2627a576e440353aaca2a9e6adc20e0f8
Stijn <stijn.vanderlinden@student.fontys.nl>
2016-10-20 11:25:49 +0200

    V0.1.2
    Features:
    - Tram Class
    - Tram Repository
    - Tram Interface

---
+++ TrinityRails/DAL/Repositories/TramRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Types;
+using DAL.Interfaces;
+
+namespace DAL.Repositories
+{
+    public class TramRepository : ITram
+    {
+        ITram context;
+        List<Tram> trams = new List<Tram>();
+        public TramRepository(ITram context)
+        {
+            this.context = context;
+        }
+        public List<Tram> getTrams()
+        {
+            return trams;
+        }
+        public void setStatus(Tram tram, Status status)
+        {
+            tram.status = status;
+            //context.setStatus(); weet niet of dit klopt
+        }
+        public void setLocation(Tram tram, int location)
+        {
+            //TODO
+            tram.location = location;
+            //context.setLocation(); weet niet of dit klopt
+        }
+    }
+}
