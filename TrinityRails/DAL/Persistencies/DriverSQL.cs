9410f5a44a602e54668b8ad8c84bd30bbed98552
Luuk van der Kallen <luuk-vdk@live.nl>
2016-10-20 14:18:45 +0200

    V0.2.2
    Features:
    AdminRepository (nog geen ingevulde methodes)
    DriverSQL (zonder query's)

---
+++ TrinityRails/DAL/Persistencies/DriverSQL.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DAL.Interfaces;
+using DAL.Types;
+
+namespace DAL.Persistencies
+{
+    public class DriverSQL
+    {
+        private DatabaseConnection databaseConnection;
+
+        public DriverSQL()
+        {
+            databaseConnection = new DatabaseConnection();
+        }
+
+        public void setCleaningTask()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setPriority()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void getLocation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TrinityRails/DAL/Repositories/DriverRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interfaces;
+using DAL.Types;
+using Classes.Enumerations;
+using Classes;
+
+namespace DAL.Repositories
+{
+    public class DriverRepository
+    {
+        IDriver context;
+
+        public DriverRepository(IDriver context)
+        {
+            this.context = context;
+        }
+
+        public void addCleaningTask(string cleaningTask)
+        {
+            
+        }
+
+        public int getLocation(int location)
+        {
+            return location;
+        }
+    }
+}
