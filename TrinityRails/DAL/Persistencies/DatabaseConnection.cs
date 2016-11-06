0560fee8cccf3d66d524d9970d67583d56a785e2
Khornath <Kevindewit@me.com>
2016-11-06 22:18:17 +0100

    V 0.2.19
    Features:
    -class "databaseConnection" contains a new method which returns a list<Int> and a new method that returns a boolean.
    
    Code Improvement:
    -alle return types in "databaseConnection" now return null if there is no result.

--- TrinityRails/DAL/Persistencies/DatabaseConnection.cs
+++ TrinityRails/DAL/Persistencies/DatabaseConnection.cs
@@ -8,8 +8,7 @@ using System.Data.SqlClient;
 namespace DAL.Persistencies
 {
     public class DatabaseConnection
-    {
-        //Er moet nog een methode komen die een list returns en een methode die een boolean returned
+    {  
         public string connectionString = @"Server = DESKTOP-NNKUBRF; Database = Tronics; User ID = sa; Password = 1234;";
 
         public void executeCommand(string query)
@@ -31,9 +30,9 @@ namespace DAL.Persistencies
             }
         }
                 
-        public int executeReaderInt(string query) //returns -1 if there is no result
+        public Nullable<int> executeReaderInt(string query) //returns null if there is no result
         {
-            int noResult = -1;
+            Nullable<int> noResult = null;
             try
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
@@ -75,6 +74,55 @@ namespace DAL.Persistencies
                 Console.WriteLine("Error: " + exception.Message);
             }
             return noResult;
-        } 
+        }
+
+        public Nullable<bool> executeReaderBool(string query) //returns null if there is no result
+        {
+            Nullable<bool> noResult = null;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    if (command.ExecuteScalar() != null)
+                    {
+                        return Convert.ToBoolean(command.ExecuteScalar());
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception.Message);
+            }
+            return noResult;
+        }
+
+        public List<int> executeReaderIntList(string query) //returns null if there is no result
+        {
+            List<int> noResult = null;
+            List<int> list = new List<int>();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    list.Clear();
+                    connection.Open();
+                    SqlCommand command = new SqlCommand(query, connection);
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        list.Add(reader.GetInt32(0));
+                    }
+                    return list;
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception.Message);
+            }
+            return noResult;  
+        }
     }
 }
