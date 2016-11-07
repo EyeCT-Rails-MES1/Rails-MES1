b763fc84c9d6e0947b147c43254a593385e2c2cf
Imre Lammers <imrelammers@live.com>
2016-11-07 12:06:43 +0100

    V 0.2.20
    Features
    Added the Driver Form

---
+++ TrinityRails/TrinityRailsDemo/DriverForm.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace TrinityRailsDemo
+{
+    partial class DriverForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbGoTo = new System.Windows.Forms.RichTextBox();
+            this.tbHistory = new System.Windows.Forms.RichTextBox();
+            this.tbRemarks = new System.Windows.Forms.RichTextBox();
+            this.cbJa = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.gbTechnicalhelp = new System.Windows.Forms.GroupBox();
+            this.cbxNeedCleaning = new System.Windows.Forms.CheckBox();
+            this.tbTramNumber = new System.Windows.Forms.RichTextBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btnRecover = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnConfirm = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblNeedTechHelp = new System.Windows.Forms.Label();
+            this.cbNee = new System.Windows.Forms.CheckBox();
+            this.gbTechnicalhelp.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbGoTo
+            // 
+            this.tbGoTo.Location = new System.Drawing.Point(38, 43);
+            this.tbGoTo.Name = "tbGoTo";
+            this.tbGoTo.Size = new System.Drawing.Size(100, 52);
+            this.tbGoTo.TabIndex = 1;
+            this.tbGoTo.Text = "";
+            // 
+            // tbHistory
+            // 
+            this.tbHistory.Location = new System.Drawing.Point(641, 43);
+            this.tbHistory.Name = "tbHistory";
+            this.tbHistory.Size = new System.Drawing.Size(100, 93);
+            this.tbHistory.TabIndex = 2;
+            this.tbHistory.Text = "";
+            // 
+            // tbRemarks
+            // 
+            this.tbRemarks.Location = new System.Drawing.Point(22, 135);
+            this.tbRemarks.Name = "tbRemarks";
+            this.tbRemarks.Size = new System.Drawing.Size(173, 59);
+            this.tbRemarks.TabIndex = 3;
+            this.tbRemarks.Text = "";
+            // 
+            // cbJa
+            // 
+            this.cbJa.AutoSize = true;
+            this.cbJa.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbJa.Location = new System.Drawing.Point(10, 52);
+            this.cbJa.Name = "cbJa";
+            this.cbJa.Size = new System.Drawing.Size(42, 21);
+            this.cbJa.TabIndex = 4;
+            this.cbJa.Text = "Ja";
+            this.cbJa.UseVisualStyleBackColor = true;
+            // 
+            // gbTechnicalhelp
+            // 
+            this.gbTechnicalhelp.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.gbTechnicalhelp.Controls.Add(this.cbNee);
+            this.gbTechnicalhelp.Controls.Add(this.lblNeedTechHelp);
+            this.gbTechnicalhelp.Controls.Add(this.cbJa);
+            this.gbTechnicalhelp.Location = new System.Drawing.Point(22, 235);
+            this.gbTechnicalhelp.Name = "gbTechnicalhelp";
+            this.gbTechnicalhelp.Size = new System.Drawing.Size(241, 100);
+            this.gbTechnicalhelp.TabIndex = 5;
+            this.gbTechnicalhelp.TabStop = false;
+            this.gbTechnicalhelp.Text = "groupBox1";
+            // 
+            // cbxNeedCleaning
+            // 
+            this.cbxNeedCleaning.AutoSize = true;
+            this.cbxNeedCleaning.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxNeedCleaning.Location = new System.Drawing.Point(22, 378);
+            this.cbxNeedCleaning.Name = "cbxNeedCleaning";
+            this.cbxNeedCleaning.Size = new System.Drawing.Size(203, 28);
+            this.cbxNeedCleaning.TabIndex = 5;
+            this.cbxNeedCleaning.Text = "Schoonmaak nodig?";
+            this.cbxNeedCleaning.UseVisualStyleBackColor = true;
+            // 
+            // tbTramNumber
+            // 
+            this.tbTramNumber.Location = new System.Drawing.Point(397, 43);
+            this.tbTramNumber.Name = "tbTramNumber";
+            this.tbTramNumber.Size = new System.Drawing.Size(100, 66);
+            this.tbTramNumber.TabIndex = 6;
+            this.tbTramNumber.Text = "";
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(329, 161);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 50);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            // 
+            // btn2
+            // 
+            this.btn2.Location = new System.Drawing.Point(410, 161);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 50);
+            this.btn2.TabIndex = 8;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(491, 161);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 50);
+            this.btn3.TabIndex = 9;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(329, 222);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 50);
+            this.btn4.TabIndex = 10;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(410, 222);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 50);
+            this.btn5.TabIndex = 11;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(491, 222);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 50);
+            this.btn6.TabIndex = 12;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(329, 278);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(75, 50);
+            this.btn7.TabIndex = 13;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(410, 278);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(75, 50);
+            this.btn8.TabIndex = 14;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(491, 278);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(75, 50);
+            this.btn9.TabIndex = 15;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            // 
+            // btnRecover
+            // 
+            this.btnRecover.Location = new System.Drawing.Point(329, 334);
+            this.btnRecover.Name = "btnRecover";
+            this.btnRecover.Size = new System.Drawing.Size(75, 50);
+            this.btnRecover.TabIndex = 16;
+            this.btnRecover.Text = "Hertel";
+            this.btnRecover.UseVisualStyleBackColor = true;
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(410, 334);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(75, 50);
+            this.btn0.TabIndex = 17;
+            this.btn0.Text = "0";
+            this.btn0.UseVisualStyleBackColor = true;
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.Location = new System.Drawing.Point(491, 334);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(75, 50);
+            this.btnConfirm.TabIndex = 18;
+            this.btnConfirm.Text = "Bevestig";
+            this.btnConfirm.UseCompatibleTextRendering = true;
+            this.btnConfirm.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(189, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "label1";
+            // 
+            // lblNeedTechHelp
+            // 
+            this.lblNeedTechHelp.AutoSize = true;
+            this.lblNeedTechHelp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNeedTechHelp.Location = new System.Drawing.Point(6, 17);
+            this.lblNeedTechHelp.Name = "lblNeedTechHelp";
+            this.lblNeedTechHelp.Size = new System.Drawing.Size(222, 20);
+            this.lblNeedTechHelp.TabIndex = 20;
+            this.lblNeedTechHelp.Text = "Technische assistentie nodig?";
+            // 
+            // cbNee
+            // 
+            this.cbNee.AutoSize = true;
+            this.cbNee.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbNee.Location = new System.Drawing.Point(10, 75);
+            this.cbNee.Name = "cbNee";
+            this.cbNee.Size = new System.Drawing.Size(53, 21);
+            this.cbNee.TabIndex = 21;
+            this.cbNee.Text = "Nee";
+            this.cbNee.UseVisualStyleBackColor = true;
+            // 
+            // DriverForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConfirm);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnRecover);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.tbTramNumber);
+            this.Controls.Add(this.cbxNeedCleaning);
+            this.Controls.Add(this.gbTechnicalhelp);
+            this.Controls.Add(this.tbRemarks);
+            this.Controls.Add(this.tbHistory);
+            this.Controls.Add(this.tbGoTo);
+            this.Name = "DriverForm";
+            this.Text = "BestuurdersForm";
+            this.gbTechnicalhelp.ResumeLayout(false);
+            this.gbTechnicalhelp.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox tbGoTo;
+        private System.Windows.Forms.RichTextBox tbHistory;
+        private System.Windows.Forms.RichTextBox tbRemarks;
+        private System.Windows.Forms.CheckBox cbJa;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.GroupBox gbTechnicalhelp;
+        private System.Windows.Forms.CheckBox cbNee;
+        private System.Windows.Forms.Label lblNeedTechHelp;
+        private System.Windows.Forms.CheckBox cbxNeedCleaning;
+        private System.Windows.Forms.RichTextBox tbTramNumber;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnRecover;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnConfirm;
+        private System.Windows.Forms.Label label1;
+    }
+}
